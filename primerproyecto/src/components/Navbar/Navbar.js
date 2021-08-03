1b8866429afd88b890610ea1004a143956ae0003
NicolasMerlini <nicolas_merlini@hotmail.com>
2021-08-03 20:38:06 -0300

    creando react

---
+++ primerproyecto/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <div className='bg-color'>
+      <h1>Este es el Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
