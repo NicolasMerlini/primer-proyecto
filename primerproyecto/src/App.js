add7ca97636f69a62a9bacc1d431a0cff23bd1b8
NicolasMerlini <nicolas_merlini@hotmail.com>
2021-08-05 19:53:17 -0300

    prueba

--- primerproyecto/src/App.js
+++ primerproyecto/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ primerproyecto/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div>
+      <h2>Este es el footer</h2>
+    </div>
+  )
+}
+
+export default footer
